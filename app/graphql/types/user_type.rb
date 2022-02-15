56b578d5e12fca120da95693a951cfc4b088cebb
Miguel Alejandro Oceguera <migueloc-c@herbalife.com>
2022-02-15 17:30:05 -0600

    Config of graphql

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String
+    field :first_name, String
+    field :last_name, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :movies_count, Integer, null: true
+
+    def movies_count
+      Object.movies.size
+    end
+  end
+end
